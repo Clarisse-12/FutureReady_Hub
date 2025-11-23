9aa6ac65591a6bb7cb96a214dc3c3a5400449abc
Clarisse-12 <c.mukayiran@alustudent.com>
2025-11-23 15:29:51 +0200

    latest edit

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+#from flask_login import UserMixin
 from datetime import datetime
 from .extentions import db
 from werkzeug.security import generate_password_hash, check_password_hash
